9aacc25067c0f340c50b79d6a8ea8e74aaff2236
Shaik Sohail <shaiksohaildnalabsindia@gmail.com>
2026-06-16 17:53:36 +0530

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,9 @@
+// Centralized Web App Infrastructure Access Variables
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDO5TlgcFHew195ivE1STKTenIeIGw2rvU",
+  authDomain: "dnaindia-9bdd7.firebaseapp.com",
+  projectId: "dnaindia-9bdd7",
+  storageBucket: "dnaindia-9bdd7.firebasestorage.app",
+  messagingSenderId: "1080314065286",
+  appId: "1:1080314065286:web:a4fe82cbb08854d839bc07"
+};
