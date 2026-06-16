d32db93822f23d96894e4862f93aae0e0872ff46
Shaik Sohail <shaiksohaildnalabsindia@gmail.com>
2026-06-16 17:50:46 +0530

    Delete js directory

--- js/config.js
+++
@@ -1,9 +0,0 @@
-// Centralized Web App Infrastructure Access Variables
-const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyDO5TlgcFHew195ivE1STKTenIeIGw2rvU",
-  authDomain: "dnaindia-9bdd7.firebaseapp.com",
-  projectId: "dnaindia-9bdd7",
-  storageBucket: "dnaindia-9bdd7.firebasestorage.app",
-  messagingSenderId: "1080314065286",
-  appId: "1:1080314065286:web:a4fe82cbb08854d839bc07"
-};
